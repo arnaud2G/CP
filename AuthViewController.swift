cdf614fa974f37018b006e34e170127de005d6d4
arnaud2G <arnaud.verrier@gmail.com>
2016-12-11 15:29:37 +0100

    - Petite passe graphique

--- AuthViewController.swift
+++ AuthViewController.swift
@@ -48,11 +48,29 @@ class AuthViewController: UIViewController {
     
     override func viewDidLoad() {
         
+        popUpStyle(view: lblAuth)
+        popUpStyle(view: btnNook)
+        popUpStyle(view: btnOk)
+        
         switch authType {
         case .location:
             lblAuth.text = "Afin de vous repérer sur la carte nous avons besoin d'avoir accès à votre localisation"
         default:
             print("Cas général")
+        }
+    }
+    
+    func popUpStyle(view:UIView) {
+        
+        if let label = view as? UILabel {
+            label.textColor = .white
+        }
+        
+        if let btn = view as? UIButton {
+            btn.setTitleColor(.white, for: .normal)
+            btn.layer.borderColor = UIColor.white.cgColor
+            btn.layer.borderWidth = 0.5
+            btn.layer.cornerRadius = 15
         }
     }
 }
