5ac02735f82a66bdda7393b3290bdd9608a1879c
arnaud2G <arnaud.verrier@gmail.com>
2016-12-11 13:52:52 +0100

    - Test sur l'utilisation de MapBox

--- CP/ViewController.swift
+++ CP/ViewController.swift
@@ -7,22 +7,56 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let point = MGLPointAnnotation()
 
+    @IBOutlet var mapView: MGLMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        mapView.delegate = self
         
+        mapView.addAnnotation(point)
         
+        updateUserLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func updateUserLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locationManager.location {
+            mapView.latitude = location.coordinate.latitude
+            mapView.longitude = location.coordinate.longitude
+            mapView.zoomLevel = 14
+            
+            point.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func mapViewRegionIsChanging(_ mapView: MGLMapView) {
+        point.coordinate = CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        point.coordinate = CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude)
+    }
 }
 
