b1acbd8e9d104afa08072ab51fcf2268e0b7dc38
arnaud2G <arnaud.verrier@gmail.com>
2016-12-11 20:27:10 +0100

    - Ajout des fonctions de geoReverse (utilisation de CLGeocoder)

--- CP/MapViewController.swift
+++ CP/MapViewController.swift
@@ -16,6 +16,7 @@ class MapViewController: UIViewController, MGLMapViewDelegate, AuthLocationManag
     let point = MGLPointAnnotation()
 
     @IBOutlet var mapView: MGLMapView!
+    let searchBar = UISearchBar()
     
     var popUpManager:PopUpManager!
     
@@ -34,7 +35,6 @@ class MapViewController: UIViewController, MGLMapViewDelegate, AuthLocationManag
         mapView.addAnnotation(point)
         
         // On ajoute la fausse searchBar
-        let searchBar = UISearchBar()
         searchBar.delegate = self
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         mapView.addSubview(searchBar)
@@ -47,15 +47,6 @@ class MapViewController: UIViewController, MGLMapViewDelegate, AuthLocationManag
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        
-        let searchAdress = SearchViewController(sender:searchBar, userRegion:MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)))
-        searchAdress.delegate = self
-        
-        popUpManager.callPopUp(presented: searchAdress, transition: UIModalTransitionStyle.crossDissolve)
-        return false
     }
     
     func retUserLocation(location: CLLocation?) {
@@ -73,12 +64,61 @@ class MapViewController: UIViewController, MGLMapViewDelegate, AuthLocationManag
         point.coordinate = CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude)
     }
     
+    func reverseGeo(location:CLLocation) {
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location, completionHandler: {
+            (placemarks, error) -> Void in
+            
+            if let placeMark = placemarks?.first {
+                
+                var reverseGeo = String()
+                
+                // Location name
+                if let locationName = placeMark.addressDictionary!["Name"] as? NSString {
+                    reverseGeo = "\(reverseGeo) - \(locationName)"
+                }
+                
+                // City
+                if let city = placeMark.addressDictionary!["City"] as? NSString {
+                    reverseGeo = "\(reverseGeo) - \(city)"
+                }
+                
+                // Zip code
+                if let zip = placeMark.addressDictionary!["ZIP"] as? NSString {
+                    reverseGeo = "\(reverseGeo) - \(zip)"
+                }
+                
+                // Country
+                if let country = placeMark.addressDictionary!["Country"] as? NSString {
+                    reverseGeo = "\(reverseGeo) - \(country)"
+                }
+                
+                self.searchBar.text = reverseGeo
+            }
+        })
+    }
+    
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         point.coordinate = CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude)
+    }
+    
+    func mapViewDidFinishRenderingMap(_ mapView: MGLMapView, fullyRendered: Bool) {
+        reverseGeo(location: CLLocation(latitude: mapView.latitude, longitude: mapView.longitude))
     }
 }
 
 extension MapViewController: SearchViewControllerProtocol {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        
+        let searchAdress = SearchViewController(sender:searchBar, userRegion:MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: mapView.latitude, longitude: mapView.longitude), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)))
+        searchAdress.delegate = self
+        
+        popUpManager.callPopUp(presented: searchAdress, transition: UIModalTransitionStyle.crossDissolve)
+        return false
+    }
+    
     func cancelSearch() {
         popUpManager.dimissPopUp()
     }
