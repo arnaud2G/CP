b4b04596a52c6e9784b6c1da57b61d430a5f978f
arnaud2G <arnaud.verrier@gmail.com>
2016-12-11 11:40:22 +0100

    Initial Commit

---
+++ CP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CP
+//
+//  Created by Arnaud Verrier on 11/12/2016.
+//  Copyright © 2016 Arnaud Verrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
